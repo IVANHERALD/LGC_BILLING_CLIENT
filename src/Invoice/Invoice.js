54b0a244c3e35f26c17bf8f030b41dc5394e6970
Ivan herald <ivanheraldd@gmail.com>
2025-02-28 15:23:01 +0530

    Refactor:The grandtotal was stored in the db without rounding off the bug has been solved

--- src/Invoice/Invoice.js
+++ src/Invoice/Invoice.js
@@ -160,7 +160,7 @@ function Invoice({ onInvoiceChange }) {
         igstAmount,
         totalTaxableValue,
         roundoffAdjustment,
-        totalGrandAmount,
+        roundedTotalGrandAmount,
         totalInWords
         
       );
